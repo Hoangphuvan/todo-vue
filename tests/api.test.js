0a74ec72ccadf410ddb8f865429a9688802d8f2c
Hoang <phuvanhoang@gmail.com>
2024-10-31 10:24:12 +0700

    add text file

---
+++ tests/api.test.js
@@ -0,0 +1,9 @@
+import { expect, test } from "vitest";
+
+test("GitHub API returns the right response", async () => {
+  const data = await fetch(
+    "https://api.github.com/repos/Hoangphuvan/todo-vue/contents/test.txt"
+  ).then((res) => res.json());
+  expect(data.name).toBe("test.txt");
+  expect(data.path).toBe("test.txt");
+});
