5b388007fb980e8b3f8d8e33c57dd23d9731a69a
Hoang <phuvanhoang@gmail.com>
2024-10-31 10:47:05 +0700

    test hellow

--- tests/api.test.js
+++ tests/api.test.js
@@ -2,7 +2,7 @@ import { expect, test } from "vitest";
 
 test("GitHub API returns the right response", async () => {
   const data = await fetch(
-    "https://api.github.com/repos/Hoangphuvan/todo-vue/contents/test.txt"
+    "https://api.github.com/repos/Hoangphuvan/todo-vue/contents/test.txt",
   ).then((res) => res.json());
   expect(data.name).toBe("test.txt");
   expect(data.path).toBe("test.txt");
