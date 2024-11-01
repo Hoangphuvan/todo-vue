61c4995d52193b7ba5b87e026f1aa10637f2a2c5
Hoang <phuvanhoang@gmail.com>
2024-11-01 22:07:46 +0700

    add new doto formt

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,50 @@
-<script setup>
+<script>
 import { nanoid } from "nanoid";
 import ToDoItem from "./components/ToDoItem.vue";
+import ToDoForm from "./components/ToDoForm.vue";
 
-const data = {
-  ToDoItems: [
-    { label: "Learn Vue", done: false, id: "todo-" + nanoid() },
-    {
-      label: "Create a Vue project with the CLI",
-      done: true,
-      id: "todo-" + nanoid(),
+export default {
+  name: "app",
+  components: {
+    ToDoItem,
+    ToDoForm,
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { id: "todo-" + nanoid(), label: "Learn Vue", done: false },
+        {
+          id: "todo-" + nanoid(),
+          label: "Create a Vue project with the CLI",
+          done: true,
+        },
+        { id: "todo-" + nanoid(), label: "Have fun", done: true },
+        {
+          id: "todo-" + nanoid(),
+          label: "Create a to-do list",
+          done: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    addToDo(toDoLabel) {
+      this.ToDoItems.push({
+        id: "todo-" + nanoid(),
+        label: toDoLabel,
+        done: false,
+      });
     },
-    { label: "Create a to-do list", done: false, id: "todo-" + nanoid() },
-  ],
+  },
 };
 </script>
 
 <template>
   <div id="app">
     <h1>To-Do List</h1>
+    <to-do-form @todo-added="addToDo"></to-do-form>
     <ul>
-      <li v-for="item in data.ToDoItems" :key="item.id">
+      <li v-for="item in ToDoItems" :key="item.id">
         <to-do-item
           :label="item.label"
           :done="item.done"
